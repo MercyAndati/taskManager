9074c40844cdf43a303f87ab52ef2195a4fa86d1
MercyAndati <andatimercy8@gmail.com>
2026-04-01 13:24:09 +0300

    Remove temporary database setup route

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
-use Illuminate\Support\Facades\Artisan;
 
 Route::get('/tasks/report',[TaskController::class, 'report']);
 Route::get('/tasks', [TaskController::class, 'index']);
@@ -10,8 +9,3 @@ Route::post('/tasks', [TaskController::class, 'store']);
 
 Route::patch('/tasks/{id}/status', [TaskController::class, 'updateStatus']);
 Route::delete('/tasks/{id}',[TaskController::class, 'destroy']);
-
-Route::get('/setup-database', function () {
-    Artisan::call('migrate', ['--force' => true]);
-    return response()->json(['message' => 'Database tables created successfully!']);
-});
