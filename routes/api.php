b8f09ece51d7763c2151e501f12d0b6c33d1bfaf
MercyAndati <andatimercy8@gmail.com>
2026-04-01 12:56:07 +0300

    Add migration route

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
+use Illuminate\Support\Facades\Artisan;
 
 Route::get('/tasks/report',[TaskController::class, 'report']);
 Route::get('/tasks', [TaskController::class, 'index']);
@@ -9,3 +10,8 @@ Route::post('/tasks', [TaskController::class, 'store']);
 
 Route::patch('/tasks/{id}/status', [TaskController::class, 'updateStatus']);
 Route::delete('/tasks/{id}',[TaskController::class, 'destroy']);
+
+Route::get('/setup-database', function () {
+    Artisan::call('migrate', ['--force' => true]);
+    return response()->json(['message' => 'Database tables created successfully!']);
+});
